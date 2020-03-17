cac0cd26927de0529dae54363e926e6d9a86ed8d
PLCoster <58193305+PLCoster@users.noreply.github.com>
2020-03-16 18:04:40 -0700

    Created import.py to insert data from books.csv into DB

---
+++ import.py
@@ -0,0 +1,21 @@
+
+# Imports Data from books.csv into books table in DB:
+import os
+import csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL")) # database engine object from SQLAlchemy that manages connections to the database
+                                                  # DATABASE_URL is an environment variable that indicates where the database lives
+db = scoped_session(sessionmaker(bind=engine))    # create a 'scoped session' that ensures different users' interactions with the
+                                                  # database are kept separate
+
+f = open("books.csv")
+reader = csv.reader(f)
+for isbn, title, author, year in reader:
+  db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {"isbn": isbn, "title": title, "author": author, "year": year}) # substitute values from CSV line into SQL command, as per this dict
+  print(f"Added book: {title} by {author}, ISNB: {isbn}, Published: {year}.")
+db.commit()
+
+print("Database Upload Complete!")
